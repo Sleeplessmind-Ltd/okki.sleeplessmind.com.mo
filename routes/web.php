9322ae6ba3701be8f93b32c44530678d5b572e26
Guillaume <guill.bout@gmail.com>
2017-10-31 18:02:09 +0800

    Remove registration functionality

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,15 @@ Route::get('/api/docs', function () {
     return view('api.docs');
 })->middleware('auth');
 
-Auth::routes();
+/**
+ * Only allow login and password reset.
+ *
+ * No registration
+ */
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
